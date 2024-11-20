82bbe7ff3d96eba3862fb9f3d4a2d9cac8ad84e3
Nahuel Amado <amado.nahuel@outlook.com>
2024-11-20 19:52:38 -0300

    section uno funcionando, resta el css del boton y la respuesta por pantalla

--- code.js
+++ code.js
@@ -1,34 +1,51 @@
 "use strict";
 
 let lvlBox = document.getElementsByClassName("lvl")[0];
-let lvl = lvlBox.value;//valores del 1 al 5
+let lvl = parseInt(lvlBox.value);//valores del 1 al 5
 let minBox = document.getElementById("min");
-let revenueMin = minBox.value;//ganancia
+let revenueMin = parseInt(minBox.value);//ganancia
 let productionsBox = document.getElementById("productions");
-let productions = productionsBox.value;
+let productions = parseInt(productionsBox.value);
 let taxesBox = document.getElementById("taxes");
-let taxes = taxesBox.value;
+let taxes = parseInt(taxesBox.value);
 let billsBox = document.getElementById("bills");
-let bills = billsBox.value;
+let bills = parseInt(billsBox.value);
 let insuranceBox = document.getElementById("insurance");
-let insurance = insuranceBox.value;
+let insurance = parseInt(insuranceBox.value);
 let rentBox = document.getElementById("rent");
-let rent = rentBox.value;
+let rent = parseInt(rentBox.value);
 let othersBox = document.getElementById("others");
-let others = othersBox.value;
+let others = parseInt(othersBox.value);
 let timeAmortizeBox = document.getElementById("amortize");
-let timeAmortize = timeAmortizeBox.value;
+let timeAmortize = parseInt(timeAmortizeBox.value);
 let poolBox = document.getElementById("pool");
-let pool = poolBox.value;
+let pool = parseInt(poolBox.value);
+let button = document.getElementsByClassName("section__div__button");
 
-lvlBox.addEventListener("change", ()=> {lvl = lvlBox.value});
-minBox.addEventListener("change", ()=> {revenueMin = minBox.value});
-productionsBox.addEventListener("change", ()=> {productions = productionsBox.value});
-taxesBox.addEventListener("change", ()=>{taxes = taxesBox.value});
-billsBox.addEventListener("change", ()=>{bills = billsBox.value});
-insuranceBox.addEventListener("change", ()=>{insurance = insuranceBox.value});
-rentBox.addEventListener("change", ()=>{rent = rentBox.value});
-othersBox.addEventListener("change", ()=>{others = othersBox.value});
-timeAmortizeBox.addEventListener("change", ()=>{timeAmortize = timeAmortizeBox.value});
-poolBox.addEventListener("change", ()=>{pool = poolBox.value});
+const calculate = ()=> {
+    let poolOfMonth = ((pool / timeAmortize) / 12);
+    console.log(poolOfMonth)
+    let moneyOfMonth = revenueMin + taxes + bills + insurance + rent + others + poolOfMonth;
+    console.log(moneyOfMonth)
+    let valueOfPRoduction = moneyOfMonth / productions;
+    return valueOfPRoduction;
+}
+
+lvlBox.addEventListener("change", ()=> {lvl = parseInt(lvlBox.value)});
+minBox.addEventListener("change", ()=> {revenueMin = parseInt(minBox.value)});
+productionsBox.addEventListener("change", ()=> {productions = parseInt(productionsBox.value)});
+taxesBox.addEventListener("change", ()=>{taxes = parseInt(taxesBox.value)});
+billsBox.addEventListener("change", ()=>{bills = parseInt(billsBox.value)});
+insuranceBox.addEventListener("change", ()=>{insurance = parseInt(insuranceBox.value)});
+rentBox.addEventListener("change", ()=>{rent = parseInt(rentBox.value)});
+othersBox.addEventListener("change", ()=>{others = parseInt(othersBox.value)});
+timeAmortizeBox.addEventListener("change", ()=>{timeAmortize = parseInt(timeAmortizeBox.value)});
+poolBox.addEventListener("change", ()=>{pool = parseInt(poolBox.value)});
+button[0].addEventListener("click", ()=>{
+    let res = calculate();
+    console.log(res)
+});
+
+
+
 
