a584b697d177dc75c03005eb3f4334733508dea9
Nahuel Amado <amado.nahuel@outlook.com>
2024-11-21 14:46:40 -0300

    boton y div de respuesta listos y mejorados. Ahora seguimos con la calcularora de fotoproducto

--- code.js
+++ code.js
@@ -21,14 +21,45 @@ let timeAmortize = parseInt(timeAmortizeBox.value);
 let poolBox = document.getElementById("pool");
 let pool = parseInt(poolBox.value);
 let button = document.getElementsByClassName("section__div__button");
+let resBox = document.getElementsByClassName("section__div--res");
 
 const calculate = ()=> {
+    let modExp, final, finalLarge;
     let poolOfMonth = ((pool / timeAmortize) / 12);
-    console.log(poolOfMonth)
     let moneyOfMonth = revenueMin + taxes + bills + insurance + rent + others + poolOfMonth;
-    console.log(moneyOfMonth)
-    let valueOfPRoduction = moneyOfMonth / productions;
-    return valueOfPRoduction;
+    let valueOfPRoductionLarge = moneyOfMonth / productions;
+    let valueOfPRoduction = valueOfPRoductionLarge.toFixed(2);
+    if (lvl === 1){
+        modExp = "- 20%";
+        finalLarge = valueOfPRoduction * 0.8;
+    } else if (lvl == 2){
+        modExp = "- 10%";
+        finalLarge = valueOfPRoduction * 0.9;
+    }  else if (lvl == 3){
+        modExp = false;
+        finalLarge = valueOfPRoduction * 1;//algo extraño pasa al quitar el *1
+    } else if (lvl == 4){
+        modExp = "+ 10%";
+        finalLarge = valueOfPRoduction * 1.1;
+    } else if (lvl == 5){
+        modExp = "+ 20%";
+        finalLarge = valueOfPRoduction * 1.2;
+    }
+    final = finalLarge.toFixed(2);
+    return [valueOfPRoduction, final, modExp];
+}
+
+const printRes = (valueOfPRoduction, final, modExp)=>{
+    let text = `Por su nivel de expertice y equipamento puede buscar estar en un ${modExp} del valor medio del mercado.`;
+    let detail = modExp === false ? "" : text;
+    let bonusText = `Se sugiere aumentar a un piso de $${final}`
+    let bonus = final > valueOfPRoduction ? bonusText : "";
+    let resText =
+        `El valor por su producción es de <b>$${valueOfPRoduction}.</b><br>
+        ${detail}<br>
+        <b>${bonus}</b>`;
+    resBox[0].innerHTML = resText;
+    resBox[0].style.setProperty("display", "block");
 }
 
 lvlBox.addEventListener("change", ()=> {lvl = parseInt(lvlBox.value)});
@@ -41,9 +72,10 @@ rentBox.addEventListener("change", ()=>{rent = parseInt(rentBox.value)});
 othersBox.addEventListener("change", ()=>{others = parseInt(othersBox.value)});
 timeAmortizeBox.addEventListener("change", ()=>{timeAmortize = parseInt(timeAmortizeBox.value)});
 poolBox.addEventListener("change", ()=>{pool = parseInt(poolBox.value)});
+
 button[0].addEventListener("click", ()=>{
-    let res = calculate();
-    console.log(res)
+    let [valueOfPRoduction, final, modExp] = calculate();
+    printRes(valueOfPRoduction, final, modExp);
 });
 
 
